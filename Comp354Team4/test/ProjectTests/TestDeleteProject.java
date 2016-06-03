ee5fd5c66e1f2a4f32115ade1aebb29b3732dc73
moh60 <mohd_theo_60@hotmail.com>
2016-06-03 14:32:29 -0400

    slight change to delete

--- Comp354Team4/test/ProjectTests/TestDeleteProject.java
+++ Comp354Team4/test/ProjectTests/TestDeleteProject.java
@@ -16,22 +16,23 @@ public class TestDeleteProject {
 	@Test 
 	public void deletePro() throws ParseException{
 		
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		/*DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		
 		Project project = new Project();
-		System.out.println("Testing Creation of a Project with all its information");
+		System.out.println("Testing Creation of a Project");
 		project.setProjectName("TestingDeletion");
 		project.setId(11);
 		project.setStartDate(df.parse("2017-02-29"));
 		project.setEndDate(df.parse("2018-02-29"));					
 		project.setManagerId(1);
-		
 		ProjectDao projectDao = new ProjectDao();
-		projectDao.InsertProject(project); //project created successfully 
+		projectDao.InsertProject(project); //project created successfully */
+		
+		ProjectDao projectDao2 = new ProjectDao();
 		System.out.println("Testing Deletion of a Project");
-		//not working... guess it only works when clicked on "delete" as an event
-		projectDao.DeleteProject(11);
-		assertNull(projectDao);//project should be null if deleted		  
+		//Project is deleted in the database but test still fails...
+		projectDao2.DeleteProject(8);
+		assertNull(projectDao2);//project should be null if deleted		  
 	} 
 	
 	
