10432dcf829d2f38f8c0e5195c5fd600ad4cc40e
moh60 <mohd_theo_60@hotmail.com>
2016-06-03 14:16:38 -0400

    delete project case

--- Comp354Team4/test/ProjectTests/TestDeleteProject.java
+++ Comp354Team4/test/ProjectTests/TestDeleteProject.java
@@ -2,13 +2,37 @@ package ProjectTests;
 
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
+
+import models.Project;
+import models.ProjectDao;
 
 public class TestDeleteProject {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	@Test 
+	public void deletePro() throws ParseException{
+		
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Project project = new Project();
+		System.out.println("Testing Creation of a Project with all its information");
+		project.setProjectName("TestingDeletion");
+		project.setId(11);
+		project.setStartDate(df.parse("2017-02-29"));
+		project.setEndDate(df.parse("2018-02-29"));					
+		project.setManagerId(1);
+		
+		ProjectDao projectDao = new ProjectDao();
+		projectDao.InsertProject(project); //project created successfully 
+		System.out.println("Testing Deletion of a Project");
+		//not working... guess it only works when clicked on "delete" as an event
+		projectDao.DeleteProject(11);
+		assertNull(projectDao);//project should be null if deleted		  
+	} 
+	
+	
 }
