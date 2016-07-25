fd7d6e3d598a68ec840c4b07b51c93e5220c821b
mouadbroos <mouadbouras@Mouads-MacBook-Air-3.local>
2016-07-25 15:29:08 -0400

    Finished up the unit tests I was missing, this commit concludes all the planned unit tests for this iteration.

--- Comp354Team4/test/AllTests.java
+++ Comp354Team4/test/AllTests.java
@@ -8,11 +8,17 @@ import ActivityTests.*;
 import ProjectTests.*;
 import GeneralApplicationTests.*;
 import ResourceTests.*;
+import UserTests.*;
+import PropertyTests.*;
+
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestCreateActivity.class, TestDeleteActivity.class, TestUpdateActivity.class,
-	TestCreateProject.class, TestDeleteProject.class, TestUpdateProject.class, TestCreateResource.class,
-	TestLogin.class, TestDatabaseConnection.class})
+@SuiteClasses({ TestCreateActivity.class, TestDeleteActivity.class, TestUpdateActivity.class,TestActivityDependency.class,
+	TestCreateProject.class, TestDeleteProject.class, TestUpdateProject.class,
+	TestCreateProperty.class,TestEditProperty.class, TestDeleteProperty.class,
+	TestCreateResource.class, TestDeleteResource.class,
+	TestCreateUser.class, TestUserMembers.class ,
+	TestLogin.class, TestDatabaseConnection.class,   })
 public class AllTests {
 
 }
--- Comp354Team4/test/PropertyTests/TestDeleteProperty.java
+++ Comp354Team4/test/PropertyTests/TestDeleteProperty.java
@@ -1,1 +1,58 @@
-//TODO
+package PropertyTests;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.junit.Test;
+
+
+import daos.PropertyDao;
+import models.Project;
+import models.Property;
+
+public class TestDeleteProperty {
+
+	private PropertyDao propertyDao = PropertyDao.getInstance();
+
+	
+	@Test
+	// testing update property name
+	public void deleteProperty() {
+
+		Property property = new Property() ;
+		
+		String name = "testName" + System.currentTimeMillis(); 
+
+		property.activityId = 1;
+		property.propertyName = name;
+		property.propertyDescription = "testDescription";
+		property.propertyText = "testText";
+
+		propertyDao.InsertProperty(property);
+		
+		property = getPropertyByActivity(name,1);
+				
+		assertTrue(propertyDao.DeleteProperty(property.id)); 
+		property = getPropertyByActivity(name,1);
+		assertEquals(getPropertyByActivity(name,1) , null );
+	}
+	
+	//performs a search for a property using activityid and UNIQUE name (THE NAME MUST BE UNIQUE FOR THIS FUNCTION TO WORK)
+	public Property getPropertyByActivity(String name , int activityId )
+	{
+		List<Property> propertyList = propertyDao.GetPropertyGivenActivityId(activityId); 
+		for (int i = 0 ; i< propertyList.size() ; i++)
+		{
+			if (propertyList.get(i).propertyName.equals(name))
+			{
+				return propertyList.get(i);
+			}
+		}		
+		return null;
+	}
+	
+}
